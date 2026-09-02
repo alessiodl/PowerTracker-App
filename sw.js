4098fb24562e572d809971549076881d44069d6b
Alessio Di Lorenzo <a.dilorenzo@izs.intra>
2026-09-02 08:25:36 +0200

    Uniforma i font con Barlow Semi Condensed e aggiorna cache Service Worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'powertrack-cache-v31';
+const CACHE_NAME = 'powertrack-cache-v32';
 
 const ASSETS_LOCAL = [
   './',
@@ -46,7 +46,7 @@ const ASSETS_LOCAL = [
 const ASSETS_CORS = [
   'https://unpkg.com/vue@3/dist/vue.global.js',
   'https://unpkg.com/dexie@3/dist/dexie.js',
-  'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&family=JetBrains+Mono:wght@400;500;600;700&display=swap'
+  'https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,700&family=JetBrains+Mono:wght@400;500;600;700;800&display=swap'
 ];
 
 const ASSETS_NO_CORS = [
